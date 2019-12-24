d20d56aae43a9077b56ecc42184f925e47bf3115
afeiship <1290657123@qq.com>
2019-12-25 00:43:29 +0800

    docs: update docs

--- public/index.js
+++ public/index.js
@@ -2,6 +2,7 @@ import ReactAntI18n from '../src/main';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import './assets/style.scss';
+import i18next from 'i18next';
 
 class App extends React.Component {
   state = {
@@ -25,7 +26,7 @@ class App extends React.Component {
           {(value) => {
             return (
               <p>
-                {value.t('hello')} - {value.t('world')}
+                {i18next.t('hello')} - {i18next.t('world')}
               </p>
             );
           }}
